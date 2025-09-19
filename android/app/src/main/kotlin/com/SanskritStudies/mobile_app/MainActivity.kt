570c33d4315531784efcbdb74007f1af2f346976
sakthivel-bpigt <sakthivel.bpigt@gmail.com>
2025-09-19 17:09:20 +0530

    this file is mandatory

---
+++ android/app/src/main/kotlin/com/SanskritStudies/mobile_app/MainActivity.kt
@@ -0,0 +1,7 @@
+package com.SanskritStudies.mobile_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+    // Leave empty unless you need to override behavior.
+}
